0047a091fa56f245714bb70cb96ca23054f2fdbf
Keillion <keillionv@gmail.com>
2024-02-01 15:55:43 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
+using SharpShell.SharpDropHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace win_drop_handler_test
+{
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.ClassOfExtension, ".txt")]//.kei-drop-test
+    class KeillionWinDropHandlerTest : SharpDropHandler
+    {
+        protected override void DragEnter(DragEventArgs dragEventArgs)
+        {
+            dragEventArgs.Effect = DragDropEffects.Link;
+        }
+
+        protected override void Drop(DragEventArgs dragEventArgs)
+        {
+            MessageBox.Show("SelectedItemPath: " + SelectedItemPath + ", DragItem: " + DragItems.FirstOrDefault());
+        }
+    }
+
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.ClassOfExtension, ".txt")]
+    class KeillionWinContextMeauTest : SharpContextMenu
+    {
+        protected override bool CanShowMenu()
+        {
+            return true;
+        }
+
+        protected override ContextMenuStrip CreateMenu()
+        {
+            var menu = new ContextMenuStrip();
+
+            var item = new ToolStripMenuItem { Text = "just test" };
+
+            menu.Items.Add(item);
+
+            return menu;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # win-drop-handler-test
+
+I write codes follow https://www.codeproject.com/Articles/512956/NET-Shell-Extensions-Shell-Context-Menus.
+
+1) build
+
+2) download ServerManager.zip in https://github.com/dwmkerr/sharpshell/releases
+
+3) open ServerManager.exe
+
+4) File -> Load Server -> choose your build out xxxx.dll
+
+5) choose one server make it blue
+
+6) Server -> Install Server(x64)
+
+7) Server -> Register Server(x64)
+
+## how to unistall
+
+1) Server -> Unregister Server(x64)
+
+2) Server -> Uninstall Server(x64)
+
+## ServerRegistrationManager
+
+ServerRegistrationManager can install dll through command line.
+
+## free the dll so you can rebuild it
+
+1) Make sure you have uninstall the dll
+
+2) Task Manager -> Windows Explorer -> Restart
